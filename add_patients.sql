d5865ea739f65b568f8258862dbe8a74da1b4cb7
zasiddiq <zasiddiq@uci.edu>
2020-09-05 19:31:59 -0700

    first commit

---
+++ add_patients.sql
@@ -0,0 +1,3 @@
+USE planetdds;
+
+INSERT IGNORE INTO patient VALUES('1', 'ZAIN', 'SIDDIQUI', '123 EASY ST', 'zain@gmail.com');
---
+++ create_table.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS planetdds;
+CREATE DATABASE planetdds;
+USE planetdds;
+
+CREATE TABLE patient (
+	id integer NOT NULL AUTO_INCREMENT,
+	firstName varchar( 50 ) NOT NULL DEFAULT '',
+	lastName varchar( 50 ) NOT NULL DEFAULT '',
+	address varchar( 200 ) NOT NULL DEFAULT '',
+	email varchar( 50 ) NOT NULL DEFAULT '',
+	PRIMARY KEY( id )
+);
+
+CREATE TABLE dentist (
+	firstName varchar( 50 ) NOT NULL DEFAULT '',
+	lastName varchar( 50 ) NOT NULL DEFAULT '',
+	address varchar( 200 ) NOT NULL DEFAULT '',
+	email varchar( 50 ) NOT NULL DEFAULT '',
+	PRIMARY KEY( email )
+);
